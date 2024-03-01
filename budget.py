339dcbace1c21a61553f97c2acde2a819ccc626c
Daniel Cross <danielcross2872@gmail.com>
2024-03-01 13:43:03 -0700

    Impliment Expense Class

--- budget.py
+++ budget.py
@@ -5,60 +5,97 @@ Editors: Elf Fire Hydrant Group
 '''
 from expense import Expense, getExpense
 
-# Individual Spending
 
-# 1000 rent, 100 parking pass
-spring_rent_24 = 1300
-summer_rent_24 = 700
-fall_rent_24 = 1300
-w_break_rent_24 = 100
-winter_rent_25 = 1300
-s_break_rent_25 = 100
-spring_rent_25 = 1300
+'''
+Spending:
 
-# 50 a week
-spring_food_24 = 700
-summer_food_24 = 350
-fall_food_24 = 700
-w_break_food_24 = 250
-winter_food_25 = 800
-s_break_food_25 = 50
-spring_food_25 = 700
+Declare Lists
+Input Amounts
+Add Up Amounts
+'''
 
-# 50 every 2 weeks, 300 every 6 months for insurance (fall and spring), 125 extra for summer
-spring_car_24 = 550
-summer_car_24 = 300
-fall_car_24 = 550
-w_break_car_24 = 150
-winter_car_25 = 350
-s_break_car_25 = 0
-spring_car_25 = 550
+# Declare Lists
 
-# Quarter Grant applied fall 24 and winter 25, internship only spring 25
-spring_tuition_24 = 2500
-summer_tuition_24 = 0
-fall_tuition_24 = 2500
-w_break_tuition_24 = 0
-winter_tuition_25 = 2500
-s_break_tuition_25 = 0
-spring_tuition_25 = 2500
+spring_spending = []
+summer_spending = []
+fall_spending = []
+w_break_spending = []
+winter_spending = []
+s_break_spending = []
+spring_spending = []
 
-# Spending by Semester
-spring_spending_24 = spring_car_24 + spring_food_24 + spring_tuition_24 + spring_rent_24
-summer_spending_24 = summer_car_24 + summer_food_24 + summer_rent_24 + summer_tuition_24
-fall_spending_24 = fall_car_24 + fall_food_24 + fall_tuition_24 + fall_rent_24
-w_break_spending_24 = w_break_car_24 + w_break_food_24 + w_break_rent_24 + w_break_tuition_24
-winter_spending_25 = winter_tuition_25 + winter_rent_25 + winter_car_25 + winter_food_25
-s_break_spending_25 = s_break_car_25 + s_break_food_25 + s_break_rent_25 + s_break_tuition_25
-spring_spending_25 = spring_car_25 + spring_food_25 + spring_tuition_25 + spring_rent_25
+
+# Input Amounts
+
+# Summer
+summer_spending.append(Expense("Rent", 700))
+summer_spending.append(Expense("Food", 350))
+summer_spending.append(Expense("Car", 300))
+
+# Fall
+fall_spending.append(Expense("Rent", 1300))
+fall_spending.append(Expense("Food", 700))
+fall_spending.append(Expense("Car", 550))
+fall_spending.append(Expense("Tuition", 2500))
+
+# Winter Break
+w_break_spending.append(Expense("Rent", 100))
+w_break_spending.append(Expense("Food", 250))
+w_break_spending.append(Expense("Car", 150))
+
+# Winter
+winter_spending.append(Expense("Rent", 1300))
+winter_spending.append(Expense("Food", 800))
+winter_spending.append(Expense("Car", 350))
+winter_spending.append(Expense("Tuition", 2500))
+
+# Spring Break
+s_break_spending.append(Expense("Rent", 100))
+s_break_spending.append(Expense("Food", 50))
+
+# Spring
+spring_spending.append(Expense("Rent", 1300))
+spring_spending.append(Expense("Food", 700))
+spring_spending.append(Expense("Car", 550))
+spring_spending.append(Expense("Tuition", 2500))
+
+
+# Add Up Amounts
+
+spring_spending_24 = 0
+summer_spending_24 = 0
+fall_spending_24 = 0
+w_break_spending_24 = 0
+winter_spending_25 = 0
+s_break_spending_25 = 0
+spring_spending_25 = 0
+
+for Expense in spring_spending:
+    spring_spending_24 += Expense.value
+
+for Expense in summer_spending:
+    summer_spending_24 += Expense.value
+
+for Expense in fall_spending:
+    fall_spending_24 += Expense.value
+
+for Expense in w_break_spending:
+    w_break_spending_24 += Expense.value
+
+for Expense in winter_spending:
+    winter_spending_25 += Expense.value
+
+for Expense in s_break_spending:
+    s_break_spending_25 += Expense.value
+
+for Expense in spring_spending:
+    spring_spending_25 += Expense.value
 
 
 '''
-Income Calculations
+Income
 '''
 
-
-# Income by Semester
 
 # 400/paycheck, minus 200/semester for leeway
 # Less in Summer and breaks for travel
@@ -71,31 +108,10 @@ s_break_income_25 = 0
 spring_income_25 = 2500
 
 
-# Function to calculate new semester incomes
-
-# Take 20% off (tax & tithing) * hrs/wk * $/hr * 14 (wks per semester)
-# Rounds to the nearest hundred
-def new_income_semester(hpw, dph):
-    return round(.8 * hpw * dph * 14, -2)
-
-# Change incomes by hrs/wk and $/hr
-
-# hpw = hours per week, dph = dollars per hour
-hpw = 0
-dph = 0
-
-if hpw != 0 and dph != 0:
-    spring_income_24 = new_income_semester(hpw, dph)
-    summer_income_24 = new_income_semester(hpw, dph) / 2
-    fall_income_24 = new_income_semester(hpw, dph)
-    winter_income_25 = new_income_semester(hpw, dph)
-    spring_income_25 = new_income_semester(hpw, dph)
-    summer_income_25 = new_income_semester(hpw, dph) / 2
-
-
 '''
 Prints out entire budget through Spring 2025
 '''
+
 
 savings_now = 10000
 after = savings_now
