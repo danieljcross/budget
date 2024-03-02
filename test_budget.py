7d1328955b5cea9593e0fef60564bf53fcf68761
David Martinez Burgos <mar2211@byui.edu>
2024-03-01 20:09:54 -0700

    test file

--- test_budget.py
+++ test_budget.py
@@ -2,3 +2,15 @@
 Test the functionality of the budget program
 Author: Elf Fire Hydrant Group
 '''
+from budget import after, spring_income_25
+import pytest
+
+def test_after():
+    assert after == -1600
+
+def test_spring_income_25():
+    assert spring_income_25 == 2500
+
+
+
+pytest.main(["-v", "--tb=line", "-rN", __file__])
